b7bdcb0ebcc6aa1f7f0e8b4cb5bc7ac99303666a
Marcel <marcelvarga201@gmail.com>
2021-03-29 22:55:18 +0100

    Detective AI using random moves

---
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Sherlock.java
@@ -0,0 +1,26 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai;
+
+import io.atlassian.fugue.Pair;
+import uk.ac.bris.cs.scotlandyard.model.Ai;
+import uk.ac.bris.cs.scotlandyard.model.Board;
+import uk.ac.bris.cs.scotlandyard.model.Move;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Sherlock implements Ai {
+    @Nonnull
+    @Override
+    public String name() {
+        return "Sherlock";
+    }
+
+    @Nonnull
+    @Override
+    public Move pickMove(@Nonnull Board board, Pair<Long, TimeUnit> timeoutPair) {
+        var moves = board.getAvailableMoves().asList();
+
+        return moves.get(new Random().nextInt(moves.size()));
+    }
+}
