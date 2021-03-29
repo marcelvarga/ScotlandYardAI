a910c1c3bdff6ee5c749bb26ca64ab8ad6fe2003
OliverG42 <ji20923@bristol.ac.uk>
2021-03-29 09:52:30 +0100

    Tried to import gameStateFactory tools without success

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/tests/DijkstraTest.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/tests/DijkstraTest.java
@@ -1,14 +1,19 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai.tests;
 
-import uk.ac.bris.cs.scotlandyard.ui.ai.Moriarty;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import uk.ac.bris.cs.scotlandyard.ui.ai.Dijkstra;
+import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
+import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
 
 class DijkstraTest {
 
-    @org.junit.jupiter.api.Test
-    public void testGetDistTo() {
-    }
+    //Create a board to test it on - no idea how to do this!
+    //GameState state = gameStateFactory.build(standard24RoundSetup(), blackPlayer(), bluePlayer);
 
     @org.junit.jupiter.api.Test
-    public void testGetDistToMrX() {
+    public void testDijkstraCreation() {
+        //Dijkstra d = new Dijkstra(board, mrXLocation, detectiveLocations);
+
     }
 }
