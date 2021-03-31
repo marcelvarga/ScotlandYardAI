3c9350333d65fff14cd14032400ff16a3eff7ddf
Marcel <marcelvarga201@gmail.com>
2021-03-31 20:24:20 +0100

    Refactored main method and constructor in Minimax Scoring function needs to be improved (only taxi moves are made)

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Minimax.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Minimax.java
@@ -11,9 +11,7 @@ import java.util.*;
 
 public class Minimax {
 
-    Board.GameState gameState;
     int steps;
-    int mrXInitialLocation;
     final int minusInfinity = -1000000;
     final int plusInfinity  = +1000000;
     Move bestMove;
@@ -23,31 +21,29 @@ public class Minimax {
     DijkstraCache dijkstraCache;
     private int maxDepth;
 
-    Minimax(Board.GameState gameState, int steps, int mrXInitialLocation, Long maxTime){
-        this.gameState = gameState;
-        this.steps = steps;
-        this.mrXInitialLocation = mrXInitialLocation;
-        this.maxTime = maxTime * 1000;
-        this.startTime = System.currentTimeMillis();
+    Minimax(){
         this.dijkstraCache = new DijkstraCache();
+        this.startTime = System.currentTimeMillis();
+
     }
 
     private int searchBestScore(Board.GameState state, int depth, int alpha, int beta, boolean isMrX, int mrXLocation) {
         // Stop searching if the depth is zero, there's a winner or the time's nearly up
-        if (depth == 0 || !state.getWinner().isEmpty() || (System.currentTimeMillis() - startTime > maxTime - 2000))
+
+        if (depth == 0)
             return score(state, mrXLocation);
+        if (!state.getWinner().isEmpty())
+            return score(state, mrXLocation);
+        if ((System.currentTimeMillis() - startTime > maxTime - 2))
+            return score(state, mrXLocation);
+
         maxDepth = Math.max(maxDepth, steps - depth + 1);
         if (isMrX) {
             int maxEval = minusInfinity;
-            ArrayList<Move> movesToCheck = movesFilter(state, mrXLocation);
+            ArrayList<Move> movesToCheck = filterMrXMoves(state, mrXLocation);
 
             // Pick one of the "best" moves to investigate first
             // Moves which increase distance tend to be better
-
-            movesToCheck = ArrayList.sort(
-                    movesToCheck,
-                    (x, y) -> leftMovesFurther(state, x, y, mrXLocation)
-            );
 
             for (Move currMove : movesToCheck)
                 if (currMove.visit(new FunctionalVisitor<>(m -> true, m -> true))) {
@@ -72,10 +68,12 @@ public class Minimax {
             return maxEval;
 
         } else {
-            int minEval = plusInfinity;
             boolean isLastPlayer = checkIfLastPlayer(state);
             int changeDepth = 0;
             if (isLastPlayer) changeDepth = 1;
+
+            int minEval = plusInfinity;
+
             ArrayList<Move> availableMoves = new ArrayList<>(state.getAvailableMoves());
             Piece currPiece = availableMoves.get(0).commencedBy();
             for (Move currMove : availableMoves)
@@ -97,22 +95,16 @@ public class Minimax {
         }
     }
 
-    private boolean leftMovesFurther(Board.GameState state, Move x, Move y, int mrXLocation) {
-        return getDistToMrX(state.advance(x), mrXLocation) > getDistToMrX(state.advance(y), mrXLocation);
-    }
-
-    private int getDistToMrX(Board.GameState state, int mrXLocation) {
-        return dijkstraCache.getDistance(state, getDetectiveLocations(state), mrXLocation);
-    }
-
     private int score(Board.GameState state, int mrXLocation){
         int distanceToMrX = dijkstraCache.getDistance(state, getDetectiveLocations(state), mrXLocation);
-
         return 100 * distanceToMrX + state.getAvailableMoves().size() + 2 * ticketFactor(state);
     }
 
-    public Move getBestMove(){
-        searchBestScore(gameState, steps, minusInfinity, plusInfinity, true, mrXInitialLocation);
+    public Move getBestMove(Board.GameState state, int steps, int mrXLocation,  Long maxTime){
+        this.maxTime = maxTime;
+        this.steps = steps;
+
+        searchBestScore(state, steps, minusInfinity, plusInfinity, true, mrXLocation);
         System.out.println("Number of verified moves: " + verifiedMoves);
         System.out.println("Looking " + maxDepth + " steps ahead");
         System.out.println("Size of Dijkstra Cache is: " + dijkstraCache.getSize());
@@ -121,12 +113,9 @@ public class Minimax {
         return bestMove;
     }
 
-    private int getDest(Move move) {
-        return move.visit(new Move.FunctionalVisitor<>(m -> m.destination, m -> m.destination2));
-    }
+    private int getDest(Move move) { return move.visit(new Move.FunctionalVisitor<>(m -> m.destination, m -> m.destination2)); }
 
-    // Checks the available moves of the state to see if there are other players left to make a move
-    // In the current round
+    // Checks the available moves of the state to see if there are other players left to make a move in the current round
     private boolean checkIfLastPlayer(Board.GameState state) {
         ArrayList<Move> moves = new ArrayList<>(state.getAvailableMoves().asList());
         if (moves.isEmpty()) return true;
@@ -137,7 +126,7 @@ public class Minimax {
         return true;
     }
 
-    private ArrayList<Move> movesFilter(Board.GameState state, int mrXLocation){
+    private ArrayList<Move> filterMrXMoves(Board.GameState state, int mrXLocation){
         ArrayList<Move> allMoves = new ArrayList<>(state.getAvailableMoves().asList());
         ArrayList<Move> temp = new ArrayList<>();
 
