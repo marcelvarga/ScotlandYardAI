81d51e1026cbf3fd22d10408bf9e7cffc062a5b9
Marcel <marcelvarga201@gmail.com>
2021-05-17 19:34:09 +0100

    Removed unnecessary methods/imports

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/tests/TestBase.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/tests/TestBase.java
@@ -6,132 +6,83 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.graph.ImmutableValueGraph;
 import com.google.common.io.Resources;
-
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import uk.ac.bris.cs.scotlandyard.model.Board;
+import uk.ac.bris.cs.scotlandyard.model.GameSetup;
+import uk.ac.bris.cs.scotlandyard.model.Model;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Transport;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
-import javax.annotation.Nonnull;
-
-import uk.ac.bris.cs.scotlandyard.model.*;
-import uk.ac.bris.cs.scotlandyard.model.Move.DoubleMove;
-import uk.ac.bris.cs.scotlandyard.model.Move.SingleMove;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Transport;
-
-import static java.util.Objects.requireNonNull;
-import static uk.ac.bris.cs.scotlandyard.model.Piece.Detective.BLUE;
-import static uk.ac.bris.cs.scotlandyard.model.Piece.Detective.GREEN;
-import static uk.ac.bris.cs.scotlandyard.model.Piece.Detective.RED;
-import static uk.ac.bris.cs.scotlandyard.model.Piece.Detective.WHITE;
-import static uk.ac.bris.cs.scotlandyard.model.Piece.Detective.YELLOW;
-import static uk.ac.bris.cs.scotlandyard.model.Piece.MrX.MRX;
-import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.DETECTIVE_LOCATIONS;
-import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.MRX_LOCATIONS;
 import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.STANDARD24ROUNDS;
-import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket.BUS;
-import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket.SECRET;
-import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket.TAXI;
-import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket.UNDERGROUND;
-import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.defaultDetectiveTickets;
-import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.defaultMrXTickets;
+import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket.*;
 import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.readGraph;
 
 /**
  * Base class for all tests. Contains various helper methods for convenience.
- * This is not a test class and contains no tests here.
+ * Copied from the model
  */
 
 @SuppressWarnings({"DefaultAnnotationParam", "SameParameterValue", "UnstableApiUsage"})
-@RunWith(Parameterized.class) abstract class TestBase {
+@RunWith(Parameterized.class)
+abstract class TestBase {
 
-    @Parameterized.Parameter(0) public ScotlandYard.Factory<Board.GameState> gameStateFactory;
-    @Parameterized.Parameter(1) public ScotlandYard.Factory<Model> modelFactory;
+    private static ImmutableValueGraph<Integer, ImmutableSet<Transport>> defaultGraph;
+    @Parameterized.Parameter(0)
+    public ScotlandYard.Factory<Board.GameState> gameStateFactory;
+    @Parameterized.Parameter(1)
+    public ScotlandYard.Factory<Model> modelFactory;
 
-    @Parameterized.Parameters(name = "{0}") public static Iterable<ScotlandYard.Factory<?>[]> data() {
+    @Parameterized.Parameters(name = "{0}")
+    public static Iterable<ScotlandYard.Factory<?>[]> data() {
         return uk.ac.bris.cs.scotlandyard.ai.ModelFactories.factories().stream()
                 .map(a -> new ScotlandYard.Factory<?>[]{a.getKey().get(), a.getValue().get()})
                 .collect(ImmutableList.toImmutableList());
     }
 
-    private static ImmutableValueGraph<Integer, ImmutableSet<Transport>> defaultGraph;
-
-    @BeforeClass public static void setUp() {
+    @BeforeClass
+    public static void setUp() {
         try {
             defaultGraph = readGraph(Resources.toString(Resources.getResource(
                     "graph.txt"),
                     StandardCharsets.UTF_8));
-        } catch (IOException e) { throw new RuntimeException("Unable to read game graph", e); }
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read game graph", e);
+        }
     }
 
     /**
      * @return the default graph used in the actual game
      */
-    @Nonnull static ImmutableValueGraph<Integer, ImmutableSet<Transport>> standardGraph() {
+    @Nonnull
+    static ImmutableValueGraph<Integer, ImmutableSet<Transport>> standardGraph() {
         return defaultGraph;
     }
 
-    @Nonnull static GameSetup standard24RoundSetup() {
+    @Nonnull
+    static GameSetup standard24RoundSetup() {
         return new GameSetup(defaultGraph, STANDARD24ROUNDS);
     }
 
     /**
-     * @return a working black player with the default tickets and default location, see
-     * {@link ScotlandYard#defaultMrXTickets} and {@link ScotlandYard#MRX_LOCATIONS}
-     */
-    @Nonnull static Player blackPlayer() {
-        return new Player(MRX, defaultMrXTickets(), MRX_LOCATIONS.get(0));
-    }
-    /**
-     * @return a working red player with the default tickets and default location, see
-     * {@link ScotlandYard#defaultDetectiveTickets} and {@link ScotlandYard#DETECTIVE_LOCATIONS}
-     */
-    @Nonnull static Player redPlayer() {
-        return new Player(RED, defaultDetectiveTickets(), DETECTIVE_LOCATIONS.get(1));
-    }
-    /**
-     * @return a working green player with the default tickets and default location, see
-     * {@link ScotlandYard#defaultDetectiveTickets} and {@link ScotlandYard#DETECTIVE_LOCATIONS}
-     */
-    @Nonnull static Player greenPlayer() {
-        return new Player(GREEN, defaultDetectiveTickets(), DETECTIVE_LOCATIONS.get(2));
-    }
-    /**
-     * @return a working blue player with the default tickets and default location, see
-     * {@link ScotlandYard#defaultDetectiveTickets} and {@link ScotlandYard#DETECTIVE_LOCATIONS}
-     */
-    @Nonnull static Player bluePlayer() {
-        return new Player(BLUE, defaultDetectiveTickets(), DETECTIVE_LOCATIONS.get(3));
-    }
-    /**
-     * @return a working yellow player with the default tickets and default location, see
-     * {@link ScotlandYard#defaultDetectiveTickets} and {@link ScotlandYard#DETECTIVE_LOCATIONS}
-     */
-    @Nonnull static Player yellowPlayer() {
-        return new Player(YELLOW, defaultDetectiveTickets(), DETECTIVE_LOCATIONS.get(4));
-    }
-    /**
-     * @return a working white player with the default tickets and default location, see
-     * {@link ScotlandYard#defaultDetectiveTickets} and {@link ScotlandYard#DETECTIVE_LOCATIONS}
-     */
-    @Nonnull static Player whitePlayer() {
-        return new Player(WHITE, defaultDetectiveTickets(), DETECTIVE_LOCATIONS.get(5));
-    }
-    /**
      * Create a map of tickets
      *
-     * @param taxi amount of tickets for {@link Ticket#TAXI}
-     * @param bus amount of tickets for {@link Ticket#BUS}
+     * @param taxi        amount of tickets for {@link Ticket#TAXI}
+     * @param bus         amount of tickets for {@link Ticket#BUS}
      * @param underground amount of tickets for {@link Ticket#UNDERGROUND}
-     * @param x2 amount of tickets for {@link Ticket#DOUBLE}
-     * @param secret amount of tickets for {@link Ticket#SECRET}
+     * @param x2          amount of tickets for {@link Ticket#DOUBLE}
+     * @param secret      amount of tickets for {@link Ticket#SECRET}
      * @return a {@link Map} with ticket counts; never null
      */
-    @Nonnull static ImmutableMap<Ticket, Integer> makeTickets(
+    @Nonnull
+    static ImmutableMap<Ticket, Integer> makeTickets(
             int taxi, int bus, int underground, int x2, int secret) {
         return ImmutableMap.of(
                 TAXI, taxi,
@@ -145,70 +96,9 @@ import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.readGraph;
      * @param rounds the reveal/hidden rounds as a boolean
      * @return a list of rounds
      */
-    @Nonnull static ImmutableList<Boolean> rounds(Boolean... rounds) {
+    @Nonnull
+    static ImmutableList<Boolean> rounds(Boolean... rounds) {
         return ImmutableList.copyOf(rounds);
     }
 
-   /**
-     * Creates a new {@link DoubleMove}
-     *
-     * @param colour colour for the move
-     * @param source the source
-     * @param first the first ticket
-     * @param firstDestination the first destination
-     * @param second the second ticket
-     * @param secondDestination the second destination
-     * @return a new double move
-     */
-    @Nonnull static DoubleMove x2(@Nonnull Piece colour, int source,
-                                  Ticket first, int firstDestination,
-                                  Ticket second, int secondDestination) {
-        return new DoubleMove(requireNonNull(colour),
-                source, requireNonNull(first), firstDestination,
-                requireNonNull(second), secondDestination);
-    }
-    /**
-     * Creates a new {@link SingleMove} of a taxi
-     *
-     * @param colour colour for the move
-     * @param source the source
-     * @param destination the destination
-     * @return a new taxi ticket move
-     */
-    @Nonnull static SingleMove taxi(@Nonnull Piece colour, int source, int destination) {
-        return new SingleMove(requireNonNull(colour), source, TAXI, destination);
-    }
-    /**
-     * Creates a new {@link SingleMove} of a secret
-     *
-     * @param colour colour for the move
-     * @param source the source
-     * @param destination the destination
-     * @return a new secret ticket move
-     */
-    @Nonnull static SingleMove secret(@Nonnull Piece colour, int source, int destination) {
-        return new SingleMove(requireNonNull(colour), source, SECRET, destination);
-    }
-    /**
-     * Creates a new {@link SingleMove} of a bus
-     *
-     * @param colour colour for the move
-     * @param source the source
-     * @param destination the destination
-     * @return a new bus ticket move
-     */
-    @Nonnull static SingleMove bus(@Nonnull Piece colour, int source, int destination) {
-        return new SingleMove(requireNonNull(colour), source, BUS, destination);
-    }
-    /**
-     * Creates a new {@link SingleMove} of an underground
-     *
-     * @param colour colour for the move
-     * @param source the source
-     * @param destination the destination
-     * @return a new underground ticket move
-     */
-    @Nonnull static SingleMove underground(@Nonnull Piece colour, int source, int destination) {
-        return new SingleMove(requireNonNull(colour), source, UNDERGROUND, destination);
-    }
 }
